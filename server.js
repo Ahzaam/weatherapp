10f5264cff711bf0d1f1832d0f2ac42dd26f72b0
Ahzaam <99543407+Ahzaam@users.noreply.github.com>
2022-09-05 00:41:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(distDir));
 
 
 app.get('/api',(req, res) => {
-    res.status(200).json({"status": 200, "message" : distDir})
+    res.status(200).json({"status": 200, "message" : distDir, "Stuck" : true})
 })
 
 app.get('/api/hello',(req, res) => {

ff434abecda411a229bed5c45fd6de55da0b6490
Ahzaam <99543407+Ahzaam@users.noreply.github.com>
2022-09-05 00:26:39 +0530

    vercel.json

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
 
+
 var distDir = __dirname + "/dist/weatherapp";
 app.use(express.static(distDir));
 
 
 app.get('/api',(req, res) => {
     res.status(200).json({"status": 200, "message" : "Hello World"})
+})
+
+app.get('/api/hello',(req, res) => {
+    res.status(200).send('I love JavaScript')
 } )
 
 
